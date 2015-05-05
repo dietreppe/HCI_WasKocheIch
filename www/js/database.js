90656479051a88d47f2a90df570e645cdb3a2ebe
Fabian <lichterschalter@gmx.at>
2015-05-05 13:16:33 +0200

    Kleiner fix in der DB

--- www/js/database.js
+++ www/js/database.js
@@ -3,38 +3,53 @@ $(document).ready(function(){
   var nameOfDB = 'Restlverwerter';
   var version = '1.0';
   var displayName = 'Restlverwerter';
-  var maxSize = 65536;
+  var maxSize = 2 * 1024 * 1024;
   db = openDatabase(nameOfDB, version, displayName, maxSize);
-    db.transaction(
-      function(transaction) {
-        transaction.executeSql(
-          'CREATE TABLE IF NOT EXISTS recipe ' +
-          '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
-          'name TEXT NOT NULL, ' +
-          'description TEXT NOT NULL, ' +
-          'image_link TEXT NOT NULL );'
-        );
-        transaction.executeSql(
-          'CREATE TABLE IF NOT EXISTS ingredient ' +
-          '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
-          'name TEXT NOT NULL, ' +
-          'favorite INT NOT NULL, ' +
-          'image_link TEXT NOT NULL ' +
-          'id_category INT NOT TULL );'
-        );
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql(
+        'CREATE TABLE IF NOT EXISTS recipe ' +
+        '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+        'name TEXT NOT NULL, ' +
+        'description TEXT NOT NULL, ' +
+        'image_link TEXT NOT NULL );'
+      );
+    }
+  );
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql(
+        'CREATE TABLE IF NOT EXISTS category ' +
+        '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+        'name TEXT NOT NULL, ' +
+        'image_link TEXT NOT NULL );'
+      );
+    }
+  );
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql(
+        'CREATE TABLE IF NOT EXISTS ingredient ' +
+        '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+        'name TEXT NOT NULL, ' +
+        'favorite INTEGER NOT NULL, ' +
+        'image_link TEXT NOT NULL, ' +
+        'id_category INTEGER NOT NULL,' +
+        'FOREIGN KEY(id_category) REFERENCES category(id));'
+      );
+    }
+  );
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql(
+        'CREATE TABLE IF NOT EXISTS recipe_has_ingredient ' +
+        '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+        'id_recipe INTEGER NOT NULL , ' +
+        'id_ingredient INTEGER NOT NULL, ' +
+        'FOREIGN KEY(id_recipe) REFERENCES recipe(id), ' +
+        'FOREIGN KEY(id_ingredient) REFERENCES ingredient(id));'
+      );
+    }
+  );
 
-        transaction.executeSql(
-          'CREATE TABLE IF NOT EXISTS category ' +
-          '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
-          'name TEXT NOT NULL, ' +
-          'image_link TEXT NOT NULL );'
-        );
-
-        transaction.executeSql(
-          'CREATE TABLE IF NOT EXISTS recipe_has_ingredient' +
-          '(id_category INTEGER NOT NULL PRIMARY KEY, ' +
-          'id_ingredient INTEGER NOT NULL PRIMARY KEY, );'
-        );
-      }
-    );
 });
